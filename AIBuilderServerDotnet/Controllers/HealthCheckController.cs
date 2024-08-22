3fbb577d56283da2b80819f59a40ca8253e2c4f8
princeVegeta2 <nellatorbeaufort@gmail.com>
2024-08-22 20:02:08 +0200

    Initial Commit

---
+++ AIBuilderServerDotnet/Controllers/HealthCheckController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AIBuilderServerDotnet.Controllers
+{
+    public class HealthCheckController : ControllerBase
+    {
+        // GET: api/healthcheck
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("HealthCheck GET is working.");
+        }
+
+        // POST: api/healthcheck
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("HealthCheck POST is working.");
+        }
+
+        // PUT: api/healthcheck
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok("Healthcheck PUT is working.");
+        }
+
+        // PATCH: api/healthcheck
+        [HttpPatch]
+        public IActionResult Patch()
+        {
+            return Ok("Healthcheck PATCH is working.");
+        }
+
+        // DELETE: api/healthcheck
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok("HealthCheck DELETE is working.");
+        }
+
+        // HEAD: api/healthcheck
+        [HttpHead]
+        public IActionResult Head()
+        {
+            return Ok("Healthcheck HEAD is working.");
+        }
+
+        // OPTIONS: api/options
+        [HttpOptions]
+        public IActionResult Options()
+        {
+            return Ok("Healthcheck OPTIONS is working.");
+        }
+    }
+}
